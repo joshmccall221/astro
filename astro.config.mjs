83737c625098bbacb7608bafb9c78982bcf5b64d
Josh McCall <joshmccall@microsoft.com>
2024-08-13 17:36:13 -0500

    https://github.com/joshmccall221/astro.git

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://joshmccall221.github.io/astro/",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
